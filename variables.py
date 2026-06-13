b3c639fc0e280f64414274f77b856982516d3de6
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 11:43:57 +0530

    Add files via upload
    variables in python value stored in memory

---
+++ variables.py
@@ -0,0 +1,33 @@
+name = "Pesi"
+age = 20
+percentage = 13.5
+isCorrect = True
+
+print(name)
+print(age)
+print(percentage)
+print(isCorrect)  
+
+
+#print single line store avvala antha ela rayandi
+
+print(f"{name}-{age}-{percentage}-{isCorrect}")
+
+
+
+#change the value of variable
+x=10
+
+x=20
+
+print(x)
+
+
+
+#---
+
+x=10
+print(x)
+
+x=20
+print(x)
