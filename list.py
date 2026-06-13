d906cd6095584c322d8539210d205cd34b3dd326
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 18:48:50 +0530

    Add files via upload
    remove,append,change,pop,clear are used in lists

---
+++ list.py
@@ -0,0 +1,37 @@
+# list in python- ordered, changeable collection using[]
+#list operations- change,append,remove,pop,clear
+
+numbers = []
+
+print(type(numbers))
+
+
+# numbers in lists
+
+numbers = [10,20,30,40]
+
+#print(numbers[0])
+
+#replace number
+#numbers[0] = 100
+#print(numbers)
+
+#append number chesta number add avvthandi
+#numbers.append(50)
+#print(numbers)
+
+#remove number
+
+#numbers.remove(20)
+#print(numbers)
+
+# pop number
+
+#numbers.pop(2)
+#print(numbers)
+
+#clear number
+
+numbers.clear()
+print(numbers)
+
