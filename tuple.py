ff6dde00045e012a93b5d72a068ab484c12037b7
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 19:08:02 +0530

    Add files via upload
    count, index, len is used as tuple

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# Tuple in python - ordered, unchangeable collection using()
+# Tuple operations - count(), index(), len(), immutability
+
+t1 = (10,20,30,30)
+
+#ordered
+
+#print(t1[1])
+
+#unchangeable collection using()
+
+# t1[0] = 100
+# print(t1)
+
+# count()
+
+#print(t1.count(30))
+
+# index()
+
+# print(t1.index(20))
+
+# len()
+
+print(len(t1))
