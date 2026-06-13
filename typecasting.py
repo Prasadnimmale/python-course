af2a349b5a0d354e43e21060d57a0e07f0cef0d5
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 12:34:18 +0530

    Add files via upload
    changed one datatype to another

---
+++ typecasting.py
@@ -0,0 +1,73 @@
+# type
+
+# int type
+x = 10
+
+print(type(x))
+
+# str type
+x = "Pesi"
+
+print(type(x))
+
+# float type
+
+x=3.12
+
+print(type(x))
+
+# bool type
+
+x= True
+
+print(type(x))
+
+
+# typecasting
+
+# float typecasting
+
+x = 10
+
+x = float(x)
+
+print(type(x))
+
+#str typecasting
+
+x = 10
+
+x = str(x)
+
+print(type(x))
+
+# value typecasting
+
+x = 10
+
+print(x)
+
+y = str(x)
+
+print(y)
+
+# x==y
+
+x = 10
+
+y = str(x)
+
+print(x == y)
+
+
+# x == z
+x = 10
+
+y=str(x)
+
+z = int(y)
+
+print(x == z)
+
+
+
