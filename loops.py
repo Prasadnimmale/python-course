3770b48289a7487c6501d95f1eee07174fc0d8b8
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 16:29:22 +0530

    Add files via upload
    using for and while loops

---
+++ loops.py
@@ -0,0 +1,74 @@
+
+
+for i in range(10):
+    print(i)
+
+# name 10 times display cheyala
+
+for i in range(10):
+    print("pesi")    
+
+# 1 to 10 display cheyala
+
+for i in range(1,11):    #(less than 11)
+    print(i)
+
+# gap 2
+
+for i in range(1,11,2):
+    print(i)
+
+# gap 3
+for i in range(1,11,3):
+    print(i)
+
+#  10 to 1 display cheyala
+#for i in range(10,0,-1):
+ #   print(i)
+
+#  for loop with if-else combination
+   # i = 1
+    
+   # if i == 2:
+   #     print("true")
+   # else:
+       # print("false")
+
+for i in range(1,6):
+    if i == 3:
+        break
+    print(i)
+
+
+#named 
+
+name = "pesi"
+
+for l in name:
+    print(l)
+
+# single line lo print cheyala
+
+name ="prasau"
+
+for l in name:
+    print(l,end=" ")
+
+
+# while loop
+
+i = 1
+
+while(i <= 5):
+    print("pesi")
+    i = i + 2
+
+
+name = input("Enter your name:")
+
+while name == "":
+    name = input("Enter your name:")
+
+    
+
+
