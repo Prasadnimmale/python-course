1f84006c1e23df5dd60e8c6adf2b05e0259b2867
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 12:05:26 +0530

    Add files via upload
    operators are arithmetic,comparsion,logical,assignment

---
+++ operators.py
@@ -0,0 +1,63 @@
+# Arithmetic operator
+
+a=10
+b=5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+
+# comparsion operator
+
+a = 11
+b= 5
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical operator(AND)
+
+a = True
+b=True
+
+print(a and b)
+
+# Note -2
+a = True
+b = False
+
+print(a and b)
+
+
+# Logical operator(OR)
+a = True
+b = False
+print(a or b)
+
+#Note -2
+a = True 
+b= True
+print(a or b)
+
+
+# Logical operator(NOT)
+a = False
+print(not a)
+
+# note -2
+a = True
+print(not a)
+
+# Assignment operator
+
+x = 5
+x = x + 3
+print(x)
+
+
