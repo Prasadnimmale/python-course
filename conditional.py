b5308847840c3accc0fe3f37c3ea026b68ca6c9d
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-13 13:17:26 +0530

    Add files via upload
    conditional statement is used is if, elif  ,else conditions.

---
+++ conditional.py
@@ -0,0 +1,10 @@
+
+
+age = int(input("Enter your age:"))
+
+if age < 10:
+    print("You are a child.")
+elif age < 18:
+    print("You are not eligible to vote.")
+else:
+    print("You are eligible to vote.")
