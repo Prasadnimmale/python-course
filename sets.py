ffab795557e3e8a170530975e25a4e5680d046f0
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-14 12:07:23 +0530

    Add files via upload
    unordered collection with duplicate elements using curly brackets

---
+++ sets.py
@@ -0,0 +1,35 @@
+# set in python -unordered collection without duplicates using {}
+# set operation - add,remove,discard, clear,union,intersection,difference
+
+#my_set = {10,20,30}
+#my_set={10,20,30,10,20,30}
+
+
+#add
+#my_set.add(40)
+
+#remove
+#my_set.remove(20)
+
+#discard
+#my_set.discard(40)
+#my_set.discard(30)
+
+#clear
+#my_set.clear()
+
+
+#union
+#a = {1,2}
+#b = {2,3}
+
+#print(a.union(b))
+
+#union c value print cheya
+a = {1,2,3,4}
+b = {2,3}
+#c = a.union(b)
+
+#print(a.intersection(b))
+print(a.difference(b))
+
