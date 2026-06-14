8f2acfefa6fbeeafec94b2f2cc5144581b466bf7
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-14 13:04:38 +0530

    Add files via upload
    key value pairs , mutuable , unordered

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+# Dictionary in python- key-value pairs,mutable,unordered
+
+# Example: {"name":"pesi"}
+# "key" is the name,"value" is the pesi
+
+
+dict1 = {"name":"pesi",
+          "age":20,
+          "college":"RITV"}
+
+
+
+#print(dict1)
+#print(dict1["name"])->Access
+
+#dict1["name"] = "prasad"->change
+#dict1["location"] = "visakhaptnam"->Add new property
+
+#dict1.pop("name") ->remove key value pair
+
+# dict1.popitem() -> remove from last
+
+#dict1.clear()
+
+print(dict1.items())
