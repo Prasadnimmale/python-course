30488326e5b933e3a8080114bab05015dad812e8
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-15 16:43:22 +0530

    Add files via upload
    directory containing mutltiple modules and *init* .py file

---
+++ main.py
@@ -0,0 +1,7 @@
+from Library import add,sub
+
+add1 = add.addition(100,200)
+
+sub1 = sub.subtraction(500,200)
+
+print(add1,sub1)
---
+++ packages.py
@@ -0,0 +1,15 @@
+# packages:
+
+# A package is a folder that contains multiple modules.
+
+# purpose: organize related modules together
+
+# A package must contain an __init__.py file(can be empty)
+
+
+# Library -> package
+# Modules -> files with .py
+#         -> __init_.py
+# .py -> functions/variables
+
+
