92b9b1e537ddfd0aff5cec2f5b0d29dc3a81423f
Prasadnimmale <prasadnimmale02@gmail.com>
2026-06-15 13:16:27 +0530

    Add files via upload
    using functions and variables

---
+++ module.py
@@ -0,0 +1,23 @@
+# Module: 
+# A module is a python file that contains functions or variables.
+# It helps us reuse code.
+# File name ends with .py.
+
+
+# module
+#import math
+
+#x = math.sqrt(16)
+#y = math.factorial(5)
+#print(x) 
+#print(y)
+
+import module1
+
+add = module1.add(10,20)
+print(add)
+
+sub = module1.sub(10,20)
+print(sub)
+
+
